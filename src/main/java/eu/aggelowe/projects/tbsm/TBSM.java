885feeb33eef4cca2e72b5a8eec4784b0915fc7b
Aggelos Margaritis <aggelos@margaritis.eu>
2021-05-20 22:24:48 +0300

    Improved gui

--- src/main/java/eu/aggelowe/projects/tbsm/TBSM.java
+++ src/main/java/eu/aggelowe/projects/tbsm/TBSM.java
@@ -1,27 +1,51 @@
 package eu.aggelowe.projects.tbsm;
 
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.SwingUtilities;
+
 import eu.aggelowe.projects.tbsm.gui.GuiManager;
+import eu.aggelowe.projects.tbsm.util.ExitStatus;
 import eu.aggelowe.projects.tbsm.util.Reference;
 
 /**
  * This class is the main class of the project and is used to call all the
- * important methods for the application to run and is being called when the program is executed.
+ * important methods for the application to run and is being called when the
+ * program is executed.
  * 
- * @author Aggelowe 
+ * @author Aggelowe
  */
 public final class TBSM {
-		
+
 	/**
 	 * This class is the main method of the project and is used to call all the
-	 * important methods for the application to run and is being called when the program is executed.
+	 * important methods for the application to run and is being called when the
+	 * program is executed.
 	 * 
 	 * @param args The given arguments when the program is executed.
 	 */
-	public static void main(String[] args) throws Throwable{
+	public static void main(String[] args) throws Throwable {
 		Reference.MAIN_LOGGER.info("Starting the application...");
 		Reference.MAIN_LOGGER.debug("Calling the gui manager...");
-		GuiManager.initGui();
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				GuiManager.initGui();				
+			}
+		});
 		Reference.MAIN_LOGGER.info("Application started successfully!");
 	}
-	
+
+	/**
+	 * This class is used to exit the application while printing the specified
+	 * messages on the console based on the status given.
+	 * 
+	 * @param status
+	 */
+	public static void exit(ExitStatus status) {
+		Reference.MAIN_LOGGER.info(status.getOutputMessage());
+		System.exit(status.getExitCode());
+		
+	}
+
 }
--- src/main/java/eu/aggelowe/projects/tbsm/gui/GuiManager.java
+++ src/main/java/eu/aggelowe/projects/tbsm/gui/GuiManager.java
@@ -1,25 +1,45 @@
 package eu.aggelowe.projects.tbsm.gui;
 
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.WindowEvent;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import eu.aggelowe.projects.tbsm.TBSM;
+import eu.aggelowe.projects.tbsm.util.ExitStatus;
 import eu.aggelowe.projects.tbsm.util.Reference;
 
 /**
  * This class is used to manage the gui of the application. The enclosed method
  * {@link #initGui()} should <b>only</b> be called by the main method as it
- * might break the interface.
+ * might break the interface.d
  * 
  * @author Aggelowe
  */
 public final class GuiManager {
 
 	static final Logger GUI_LOGGER = LogManager.getLogger("GuiManager");
-	
-	static final JFrame FRAME = new JFrame();
-	
+	static final JPanel MAINFRAME = new JPanel();
+	static final JFrame WINDOW = new JFrame() {
+
+		protected void processWindowEvent(WindowEvent e) {
+			if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+				dispose();
+				TBSM.exit(ExitStatus.GRACEFUL);
+			}
+		}
+
+		private static final long serialVersionUID = -1943892997658496084L;
+
+	};
+
 	/**
 	 * This method is being called at the start of the application and is used to
 	 * launch the user interface. This method should <b>only</b> be called by the
@@ -28,16 +48,51 @@ public final class GuiManager {
 	 */
 	public static void initGui() {
 		GUI_LOGGER.info("Starting drawing gui...");
-		setupFrame();
+		GUI_LOGGER.debug("Creating mainframe...");
+		GuiManager.setupWindow();
+		MAINFRAME.add(new JLabel(ComponentReference.APP_LOGO));
 		GUI_LOGGER.debug("Showing frame...");
-		FRAME.setVisible(true);
+		WINDOW.setVisible(true);
 		GUI_LOGGER.info("Gui drawn succesgully!");
 	}
-	
-	private static void setupFrame() {
+
+	/**
+	 * This method is used to setup the frame/window in order to make it work and
+	 * able to cooperate with the other components. This method should <b>only</b>
+	 * be called by the {@link #initGui()} method as it might interfere with the
+	 * application's appearance or even cause component duplications.
+	 */
+	private static void setupWindow() {
 		GUI_LOGGER.debug("Creating frame...");
-		FRAME.setTitle(Reference.FULL_NAME);
-		FRAME.setName("Main frame");
-		FRAME.setSize(960, 540);
+		WINDOW.setTitle(Reference.FULL_NAME);
+		WINDOW.setName("Main frame");
+		WINDOW.setSize(ComponentReference.DEFAULT_WINDOW_SIZE);
+		WINDOW.setMinimumSize(ComponentReference.MINIMUM_WINDOW_SIZE);
+		WINDOW.getContentPane().add(MAINFRAME);
+		WINDOW.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		WINDOW.setFont(Font.getFont("Monospaced Bold"));
+	}
+
+	/**
+	 * This class contains a lot of very important variables of the application such
+	 * as the default size of the window or frame layouts.
+	 */
+	private static final class ComponentReference {
+
+		/**
+		 * The window's default size.
+		 */
+		public static final Dimension DEFAULT_WINDOW_SIZE = new Dimension(960, 540);
+
+		/**
+		 * The window's minimum size.
+		 */
+		public static final Dimension MINIMUM_WINDOW_SIZE = new Dimension(720, 405);
+		
+		/**
+		 * The application's logo.
+		 */
+		public static final ImageIcon APP_LOGO = new ImageIcon(Thread.currentThread().getContextClassLoader().getResource("assets/icon.png"));
+
 	}
 }
